6db1c50ec772e0580f9beb152a7d44e3eca19496
Alexandr Minakov <am@infodrive.pro>
2023-11-08 21:47:57 +0300

    Добавил сортировку пересчетом

---
+++ CountingSort/Program.cs
@@ -0,0 +1,26 @@
+﻿//Сортировка подсчетом
+
+int[] array = { 0, 2, 3, 2, 1, 5, 9, 1, 1 };
+
+CountingSort(array);
+
+Console.WriteLine(string.Join(", ", array));
+
+void CountingSort(int[] inputArray)
+{
+    int[] counters = new int[10]; //массив повторений
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i]]++;
+    }
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            inputArray[index] = i;
+            index++;
+        }
+    }
+}
